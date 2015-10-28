3a91a99cf35567e18c77cffd4c177bca483c6fb6
jaythaceo <jaythaceo@gmail.com>
2015-10-28 17:39:36 -0400

    [chore] Added Test for titles

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -5,15 +5,18 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
+    assert_select "title", "Home | Kent Blake Realty"
   end
 
   test "should get help" do
     get :help
     assert_response :success
+    assert_select "title", "Help | Kent Blake Realty"
   end
 
   test "should get about" do
     get :about
     assert_response :success
+    assert_select "title", "About | Kent Blake Realty"
   end
 end
